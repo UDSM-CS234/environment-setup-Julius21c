283ac49d6e840d6a471d6c90a3fc0879370db1bc
Julius21c <juliuskawata@gmail.com>
2022-05-19 09:44:05 -0700

    LAB1 Assignment

---
+++ src/position.java
@@ -0,0 +1,13 @@
+public class position {
+        public  static void  main(String[]args){
+            double a=-9.81;
+            double t=10;
+            double v1=0;
+            double x1=0;
+            double Xt;
+            Xt=0.5*a*t*t+v1*t*x1;
+            System.out.println("the value of Xt is"+Xt +"m");
+        }
+    }
+
+
